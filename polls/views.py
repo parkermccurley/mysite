6a4cf743fab3340cbcb312691037cdd86a57bd48
Parker McCurley <parker@decentlabs.io>
2020-04-28 22:56:34 -0400

    Add 404 to detail page

--- polls/views.py
+++ polls/views.py
@@ -1,20 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
+from django.shortcuts import get_object_or_404, render
 from django.template import loader
 
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list
-    }
-    return HttpResponse(template.render(context, request))
+    context = { 'latest_question_list': latest_question_list }
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    response = "You're looking at question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = { 'question': question }
+    return render(request, 'polls/detail.html', context)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
